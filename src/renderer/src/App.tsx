0022a24e3fce3565c4b004f2d7d0990e3162070c
shota <sspydery0@gmail.com>
2024-10-10 10:07:52 +0900

    refactor: format code

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,8 +1,8 @@
-import Versions from "./components/Versions";
-import electronLogo from "./assets/electron.svg";
+import Versions from "./components/Versions"
+import electronLogo from "./assets/electron.svg"
 
 function App(): JSX.Element {
-  const ipcHandle = (): void => window.electron.ipcRenderer.send("ping");
+  const ipcHandle = (): void => window.electron.ipcRenderer.send("ping")
 
   return (
     <>
@@ -28,7 +28,7 @@ function App(): JSX.Element {
       </div>
       <Versions />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
