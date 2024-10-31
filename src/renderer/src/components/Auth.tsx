f08eb983a5a9262345d6f94591abf51d3c706a5a
shota <sspydery0@gmail.com>
2024-10-31 17:55:36 +0900

    feat: modify package.json

--- src/renderer/src/components/Auth.tsx
+++ src/renderer/src/components/Auth.tsx
@@ -3,5 +3,9 @@ import { Login } from "../pages/login/Login"
 
 export const Auth = ({ children }: { children: JSX.Element }) => {
   const { loggedIn } = useAuthContext()
-  return loggedIn ? children : <Login />
+  // ログインしていない場合はログインページにリダイレクト
+  if (!loggedIn) {
+    return <Login />
+  }
+  return children
 }
